c4f849c0a6e17ce91f3af5ff75065bebce2c1c1f
Oliver Götz <goetzoliver89@gmail.com>
2022-01-31 15:16:41 +0100

    init commit

--- sns.tf
+++ sns.tf
@@ -1,5 +1,6 @@
 resource "aws_sns_topic" "report" {
-  name = "${local.resource_prefix}SNS-ReportTopic"
+  name              = "${local.resource_prefix}SNS-ReportTopic"
+  kms_master_key_id = var.kms_key_alias ? data.kms_key_alias.this[0].name : "alias/aws/sns"
 }
 
 resource "aws_sns_topic_subscription" "email" {
